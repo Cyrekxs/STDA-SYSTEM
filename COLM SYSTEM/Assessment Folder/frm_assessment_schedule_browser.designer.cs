bcf92b75e30604f7df35cf8df3d4524986a0f231
Anthony Quijano <anthonyquijano20@gmail.com>
2023-08-24 08:40:39 +0800

    Added ignored files

---
+++ COLM SYSTEM/Assessment Folder/frm_assessment_schedule_browser.designer.cs
@@ -0,0 +1,216 @@
+﻿namespace COLM_SYSTEM.Assessment_Folder
+{
+    partial class frm_assessment_schedule_browser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clmScheduleID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCourse = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmYearLevel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmDay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmTimeIn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmTimeOut = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmRoom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmFaculty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmPick = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmScheduleID,
+            this.clmCourse,
+            this.clmYearLevel,
+            this.clmDay,
+            this.clmTimeIn,
+            this.clmTimeOut,
+            this.clmRoom,
+            this.clmFaculty,
+            this.clmPick});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(679, 394);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Subject Description";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(13, 29);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(681, 23);
+            this.txtSubject.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.txtSubject);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 467);
+            this.panel1.TabIndex = 3;
+            // 
+            // clmScheduleID
+            // 
+            this.clmScheduleID.HeaderText = "SCHEDULE ID";
+            this.clmScheduleID.Name = "clmScheduleID";
+            this.clmScheduleID.ReadOnly = true;
+            this.clmScheduleID.Visible = false;
+            // 
+            // clmCourse
+            // 
+            this.clmCourse.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmCourse.HeaderText = "Course";
+            this.clmCourse.Name = "clmCourse";
+            this.clmCourse.ReadOnly = true;
+            this.clmCourse.Width = 70;
+            // 
+            // clmYearLevel
+            // 
+            this.clmYearLevel.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmYearLevel.HeaderText = "Year Level";
+            this.clmYearLevel.Name = "clmYearLevel";
+            this.clmYearLevel.ReadOnly = true;
+            this.clmYearLevel.Width = 85;
+            // 
+            // clmDay
+            // 
+            this.clmDay.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmDay.HeaderText = "Day";
+            this.clmDay.Name = "clmDay";
+            this.clmDay.ReadOnly = true;
+            this.clmDay.Width = 53;
+            // 
+            // clmTimeIn
+            // 
+            this.clmTimeIn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmTimeIn.HeaderText = "Time In";
+            this.clmTimeIn.Name = "clmTimeIn";
+            this.clmTimeIn.ReadOnly = true;
+            this.clmTimeIn.Width = 72;
+            // 
+            // clmTimeOut
+            // 
+            this.clmTimeOut.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmTimeOut.HeaderText = "Time Out";
+            this.clmTimeOut.Name = "clmTimeOut";
+            this.clmTimeOut.ReadOnly = true;
+            this.clmTimeOut.Width = 81;
+            // 
+            // clmRoom
+            // 
+            this.clmRoom.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.clmRoom.HeaderText = "Room";
+            this.clmRoom.Name = "clmRoom";
+            this.clmRoom.ReadOnly = true;
+            this.clmRoom.Width = 63;
+            // 
+            // clmFaculty
+            // 
+            this.clmFaculty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmFaculty.HeaderText = "Faculty";
+            this.clmFaculty.Name = "clmFaculty";
+            this.clmFaculty.ReadOnly = true;
+            // 
+            // clmPick
+            // 
+            this.clmPick.ActiveLinkColor = System.Drawing.Color.DarkSlateGray;
+            this.clmPick.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.clmPick.HeaderText = "Pick";
+            this.clmPick.LinkColor = System.Drawing.Color.DarkSlateGray;
+            this.clmPick.Name = "clmPick";
+            this.clmPick.ReadOnly = true;
+            this.clmPick.Text = "PICK";
+            this.clmPick.UseColumnTextForLinkValue = true;
+            this.clmPick.VisitedLinkColor = System.Drawing.Color.DarkSlateGray;
+            this.clmPick.Width = 36;
+            // 
+            // frm_assessment_schedule_browser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 496);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_assessment_schedule_browser";
+            this.Text = "SCHEDULE BROWSER";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmScheduleID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCourse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmYearLevel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTimeIn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTimeOut;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmRoom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmFaculty;
+        private System.Windows.Forms.DataGridViewLinkColumn clmPick;
+    }
+}
